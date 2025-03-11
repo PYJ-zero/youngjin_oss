dc003b0f1a07236e3ac11f2041f0d95c4d938daa
PYJ-zero <102598576+PYJ-zero@users.noreply.github.com>
2025-03-11 04:39:43 +0000

    add helm repo

--- argocd/install.sh
+++ argocd/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #!/usr/local/bin/kubectl
-
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
 kubectl create ns argocd
 helm install argocd argo/argo-cd -f values.yaml --namespace argocd
 
